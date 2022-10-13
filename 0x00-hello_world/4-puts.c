8d88b7c541573e7c4a990b8000e571b749c9cd41
Jude odika <judeodika@gmail.com>
2022-10-13 12:36:45 +0100

    Update file 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-/*
- *  * main - main block
+/**
+ *  * main - the block of code
  *   * Return: exit
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
