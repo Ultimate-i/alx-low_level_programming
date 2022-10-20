8c16d98af9f002158528405638c8b56ed3c6d6a6
Jude odika <judeodika@gmail.com>
2022-10-20 18:22:07 +0100

    Update files 4 & 5

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,19 +3,19 @@
 /**
  * print_most_numbers - Print numbers 0 to 9, excluding 2 and 4.
  * Description: Can only use _putchar twice.
-*/
+ */
 
 void print_most_numbers(void)
 {
 	int i;
 
-	i = 0;
-	while (i < 10)
+	for (i = 0; i < 10; i++)
 	{
 		if (i != 2 && i != 4)
+		{
 			_putchar(i + '0');
-
-			i++;
+		}
 	}
+
 	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,20 +3,25 @@
 /**
  * more_numbers - prints 10 times the numbers, from 0 to 14
  * followed by a new line
-*/
+ */
 
 void more_numbers(void)
 {
-	int i, j;
+	int c, i;
 
-	for (i = 0; i < 10; i++)
+	c = 0;
+	while (c < 10)
 	{
-		for (j = 0; j < 15; j++)
+		i = 0;
+		while (i < 15)
 		{
-			if (j >= 10)
-				_putchar(j / 10 + '0');
-				_putchar(j % 10 + '0');
+			if (i > 9)
+				_putchar(i / 10 + '0');
+
+			_putchar(i % 10 + '0');
+			i++;
 		}
+		c++;
 		_putchar('\n');
 	}
 }
